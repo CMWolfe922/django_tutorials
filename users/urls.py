ea0315cceec58dec5eaa524b9ac4b4906b3d38dd
C M Wolfe <cmwolfe.dev@gmail.com>
2022-08-10 04:03:53 -0500

    Added a favicon.png img

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # Now create my new login and logout paths:
     path('login', views.custom_login, name="login"),
     path('logout', views.custom_logout, name="logout"),
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
